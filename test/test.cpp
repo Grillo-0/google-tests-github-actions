ab42422d022dce581c04a40c14302a192515c534
Arthur Grillo <arthur.grillo@usp.br>
2021-09-29 17:52:45 -0300

    changed test

--- test/test.cpp
+++ test/test.cpp
@@ -3,5 +3,11 @@
 
 TEST(soma, positivos)
 {
-    EXPECT_EQ(8, soma(4, 2));
+    EXPECT_EQ(6, soma(4, 2));
+}
+
+TEST(soma, negativos)
+{
+    EXPECT_EQ(1, soma(-1, 2));
+    EXPECT_EQ(-2, soma(-1, -1));
 }
